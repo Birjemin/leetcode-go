b4b32bd313f6900ec045e9f152e313f4a4e405d4
birjemin <birjemin@foxmail.com>
2020-01-02 16:22:10 +0800

    0038

--- algorithms/0038-count-and-say/count-and-say_test.go
+++ algorithms/0038-count-and-say/count-and-say_test.go
@@ -21,30 +21,30 @@ type question struct {
 func Test(t *testing.T) {
     ast := assert.New(t)
     qs := []question{
-        //{
-        //    p: param{
-        //        one: 1,
-        //    },
-        //    a: result{
-        //        one: "1",
-        //    },
-        //},
-        //{
-        //   p: param{
-        //       one: 2,
-        //   },
-        //   a: result{
-        //       one: "11",
-        //   },
-        //},
-        //{
-        //   p: param{
-        //       one: 4,
-        //   },
-        //   a: result{
-        //       one: "1211",
-        //   },
-        //},
+        {
+           p: param{
+               one: 1,
+           },
+           a: result{
+               one: "1",
+           },
+        },
+        {
+          p: param{
+              one: 2,
+          },
+          a: result{
+              one: "11",
+          },
+        },
+        {
+          p: param{
+              one: 4,
+          },
+          a: result{
+              one: "1211",
+          },
+        },
         {
            p: param{
                one: 7,
@@ -53,14 +53,14 @@ func Test(t *testing.T) {
                one: "13112221",
            },
         },
-        //{
-        //    p: param{
-        //        one: 10,
-        //    },
-        //    a: result{
-        //        one: "13211311123113112211",
-        //    },
-        //},
+        {
+           p: param{
+               one: 10,
+           },
+           a: result{
+               one: "13211311123113112211",
+           },
+        },
     }
     for _, q := range qs {
         a, p := q.a, q.p

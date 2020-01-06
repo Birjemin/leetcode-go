7e3fa3dfb6ad5f15eb0f35a9d3a3cbe427101880
birjemin <birjemin@foxmail.com>
2020-01-06 10:38:47 +0800

    add 0066

--- algorithms/0066-plus-one/plus-one.go
+++ algorithms/0066-plus-one/plus-one.go
@@ -6,7 +6,7 @@ func plusOne(digits []int) []int {
         // 当前位不会产生进位
         if digits[i] < 9 {
             // 如果i = len(digits) - 1则代表+1，反之代表进位1
-            digits[i] += 1
+            digits[i]++
             return digits
         }
         // 如果digits[i] == 9 则产生进位1，当前位为0
